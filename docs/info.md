1dbaa4163e8924426edeec5113ad04db0822a5e8
htfab <mpw@htamas.net>
2024-10-18 05:11:10 +0200

    Add sources

--- docs/info.md
+++ docs/info.md
@@ -9,12 +9,14 @@ You can also include images in this folder and reference them in the markdown. E
 
 ## How it works
 
-Explain how your project works
+Provides a P latch and an N latch to test if they can be hardened correctly.
 
 ## How to test
 
-Explain how to use your project
+The P latch should transparently pass through `P_D` to `P_Q` when `P_E` is high and keep its state when `P_E` is low.
+
+The N latch should transparently pass through `N_D` to `N_Q` when `N_E` is low and keep its state when `N_E` is high.
 
 ## External hardware
 
-List external hardware used in your project (e.g. PMOD, LED display, etc), if any
+None
--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_htfab_latch_test (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,12 +16,36 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+// positive latch
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+wire p_e = ui_in[0];
+wire p_d = ui_in[1];
+reg p_q;
+
+always_latch begin
+    if (p_e) p_q = p_d;
+end
+
+// negative latch
+
+wire n_e = ui_in[2];
+wire n_d = ui_in[3];
+reg n_q;
+
+always_latch begin
+    if (~n_e) n_q = n_d;
+end
+
+// assign outputs
+
+assign uo_out[0] = p_q;
+assign uo_out[1] = n_q;
+assign uo_out[7:2] = 0;
+assign uio_out = 0;
+assign uio_oe = 0;
+
+// ignore unused inputs
+
+wire _unused = &{ena, clk, rst_n, ui_in[7:4], uio_in, 1'b0};
 
 endmodule
